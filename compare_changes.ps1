dab42d79ba3b1570a5237cbf2f4b6a31f1585af5
Richard Meitern <richard.meitern@ut.ee>
2024-02-20 09:02:24 +0200

    measure diff

--- compare_changes.ps1
+++ compare_changes.ps1
@@ -6,21 +6,22 @@ $commit2 = "264e602cfb94573ab766d28a5aa23cb0a7fc5126"
 $diffStats = git diff --shortstat $commit1 $commit2
 
 # Parse the output
-$added = $diffStats -replace '.*?(\d+) insertions.*', '$1'
+$added_diff = $diffStats -replace '.*?(\d+) insertions.*', '$1'
 $changed = $diffStats -replace '.*?(\d+) deletions.*', '$1'
 
 
 # Get the total number of lines at a specific commit
-git checkout $commit2
+git checkout -q $commit2
 $totalLinesCommit2 = (git ls-files | foreach { Get-Content $_ } | Measure-Object -line).Lines
 
 # Get the total number of lines at the latest commit
-git checkout $commit1
+git checkout -q $commit1
 $totalLinesCommit1 = (git ls-files | foreach { Get-Content $_ } | Measure-Object -line).Lines
 
 # Output the total lines
 Write-Output "Total lines at commit ${commit2}: $totalLinesCommit2"
 Write-Output "Total lines at commit ${commit1}: $totalLinesCommit1"
+$added = $totalLinesCommit1 - $totalLinesCommit2
 
 # Calculate percentages
 $addedPercent = [Math]::Round(($added / $totalLinesCommit2) * 100)
