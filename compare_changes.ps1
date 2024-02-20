e7591ab5844c673a4d3f0c67c0f03d5deca9a47d
Richard Meitern <richard.meitern@ut.ee>
2024-02-20 09:19:23 +0200

    measure diff

--- compare_changes.ps1
+++ compare_changes.ps1
@@ -1,6 +1,18 @@
-# Define the two commits
+# Define the first commit
 $commit1 = "master"
-$commit2 = "264e602cfb94573ab766d28a5aa23cb0a7fc5126"
+
+# Define the default date
+$defaultDate = "2024-01-01"
+
+# Check if a date argument was provided
+if ($args.Length -eq 0) {
+    $date = $defaultDate
+} else {
+    $date = $args[0]
+}
+
+# Get the last commit before a specific date
+$commit2 = git rev-list -1 --before="$date" $commit1
 
 # Get the diff stats
 $diffStats = git diff --shortstat $commit1 $commit2
@@ -8,7 +20,6 @@ $diffStats = git diff --shortstat $commit1 $commit2
 # Parse the output
 $added_diff = $diffStats -replace '.*?(\d+) insertions.*', '$1'
 $changed = $diffStats -replace '.*?(\d+) deletions.*', '$1'
-
 
 # Get the total number of lines at a specific commit
 git checkout -q $commit2
@@ -18,8 +29,11 @@ $totalLinesCommit2 = (git ls-files | foreach { Get-Content $_ } | Measure-Object
 git checkout -q $commit1
 $totalLinesCommit1 = (git ls-files | foreach { Get-Content $_ } | Measure-Object -line).Lines
 
+#Output comparsion date
+Write-Host "Changes since $date" -ForegroundColor Green
+
 # Output the total lines
-Write-Output "Total lines at commit ${commit2}: $totalLinesCommit2"
+Write-Output "Total lines at commit ${date}: $totalLinesCommit2"
 Write-Output "Total lines at commit ${commit1}: $totalLinesCommit1"
 $added = $totalLinesCommit1 - $totalLinesCommit2
 
@@ -35,11 +49,5 @@ Write-Output "Percentage of lines added: $addedPercent%"
 Write-Output "Percentage of lines changed: $changedPercent%"
 Write-Output "Percentage of lines changed or added: $changedOrAddedPercent%"
 
-
-
-#color the last output
-Write-Host "Retained orignal: $retainedOriginal%" -ForegroundColor Green
-
-
-
-
+# Color the last output
+Write-Host "Retained original: $retainedOriginal%" -ForegroundColor Green
