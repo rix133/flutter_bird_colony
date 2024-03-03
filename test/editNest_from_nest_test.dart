33c8d04d6e4f7a470b069a278ee1f548d311e9f8
Richard Meitern <richard.meitern@ut.ee>
2024-03-03 22:10:18 +0200

    added egg tests #37

--- test/editNest_from_nest_test.dart
+++ test/editNest_from_nest_test.dart
@@ -1,10 +1,13 @@
 
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kakrarahu/design/speciesRawAutocomplete.dart';
+import 'package:kakrarahu/models/firestore/egg.dart';
+import 'package:kakrarahu/screens/nest/editEgg.dart';
 import 'package:kakrarahu/screens/nest/findNest.dart';
 
 import 'package:kakrarahu/screens/homepage.dart';
@@ -32,12 +35,21 @@ void main() {
     id: "1",
     coordinates: GeoPoint(0, 0),
     accuracy: "12.22m",
-    last_modified: DateTime.now(),
-    discover_date: DateTime.now(),
+    last_modified: DateTime.now().subtract(Duration(days: 1)),
+    discover_date: DateTime.now().subtract(Duration(days: 2)),
+    first_egg: DateTime.now().subtract(Duration(days: 2)),
     responsible: "Admin",
     species: "Common gull",
     measures: [Measure.note()],
   );
+  final Egg egg = Egg(
+      id: "1 egg 1",
+      discover_date: DateTime.now().subtract(Duration(days: 2)),
+      responsible: "Admin",
+      ring: null,
+      last_modified: DateTime.now().subtract(Duration(days: 1)),
+      status: "intact",
+      measures: [Measure.note()]);
 
   setUpAll(() async {
     AuthService.instance = authService;
@@ -65,6 +77,18 @@ void main() {
                 firestore: firestore,
               ),
             );
+
+          }
+          else if (settings.name == '/editEgg') {
+            return MaterialPageRoute(
+              builder: (context) => EditEgg(
+                firestore: firestore,
+              ),
+              settings: RouteSettings(
+                arguments: egg, // get initial nest from object
+              ),
+            );
+
           }
           // Other routes...
           return MaterialPageRoute(
@@ -81,6 +105,8 @@ void main() {
     //reset the database
     await firestore.collection('recent').doc("nest").set({"id":"1"});
     await firestore.collection(DateTime.now().year.toString()).doc(nest.id).set(nest.toJson());
+    //add egg to nest
+    await firestore.collection(DateTime.now().year.toString()).doc(nest.id).collection("egg").doc(egg.id).set(egg.toJson());
 
   });
 
@@ -122,5 +148,67 @@ void main() {
     // Verify if the TextField's controller's text is "Common gull"
     expect(textField.controller?.text, "Common gull");
   });
+
+   testWidgets("will have one egg listed", (WidgetTester tester) async {
+    await tester.pumpWidget(myApp);
+    await tester.pumpAndSettle();
+
+
+    //for debugging purposes its good to print out all the widgets texts
+    tester.allWidgets.where((widget) => widget is ElevatedButton).forEach((widget) {
+      final ElevatedButton button = widget as ElevatedButton;
+      if (button.child is Text) {
+        final buttonText = (button.child as Text).data;
+        print(buttonText);
+      }
+    });
+
+    expect(find.text('Egg 1 intact 2 days old'), findsOneWidget);
+  });
+
+    testWidgets("will navigate to edit egg", (WidgetTester tester) async {
+      await tester.pumpWidget(myApp);
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('Egg 1 intact 2 days old'));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(EditEgg), findsOneWidget);
+      expect(find.text("Nest: 1 egg 1"), findsOneWidget);
+    });
+
+testWidgets("will navigate to nest when egg is saved", (WidgetTester tester) async {
+      await tester.pumpWidget(myApp);
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('Egg 1 intact 2 days old'));
+      await tester.pumpAndSettle();
+
+
+      await tester.tap(find.byIcon(Icons.save));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(NestManage), findsOneWidget);
+    });
+
+ testWidgets("will navigate to nest when egg is deleted", (WidgetTester tester) async {
+        await tester.pumpWidget(myApp);
+        await tester.pumpAndSettle();
+
+        await tester.tap(find.text('Egg 1 intact 2 days old'));
+        await tester.pumpAndSettle();
+
+        await tester.tap(find.byIcon(Icons.delete));
+        await tester.pumpAndSettle();
+
+        expect(find.text("Removing item"), findsOneWidget);
+
+        await tester.tap(find.text('Delete'));
+        await tester.pumpAndSettle();
+
+        expect(find.byType(NestManage), findsOneWidget);
+        expect(find.text('Egg 1 intact 2 days old'), findsNothing);
+        expect(firestore.collection(DateTime.now().year.toString()).doc(nest.id).collection("egg").doc(egg.id).get(), completion((DocumentSnapshot snapshot) => snapshot.exists == false));
+      });
 
 }
