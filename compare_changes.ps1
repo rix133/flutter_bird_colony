3cce1239fc3767d53741c59d426b60d0e91af270
Richard Meitern <richard.meitern@ut.ee>
2024-03-03 22:34:52 +0200

    fixed calc

--- compare_changes.ps1
+++ compare_changes.ps1
@@ -21,6 +21,7 @@ $diffStats = git diff --shortstat $commit1 $commit2
 $added_diff = $diffStats -replace '.*?(\d+) insertions.*', '$1'
 $changed = $diffStats -replace '.*?(\d+) deletions.*', '$1'
 
+
 # Get the total number of lines at a specific commit
 git checkout -q $commit2
 $totalLinesCommit2 = (git ls-files | foreach { Get-Content $_ } | Measure-Object -line).Lines
@@ -32,24 +33,15 @@ $totalLinesCommit1 = (git ls-files | foreach { Get-Content $_ } | Measure-Object
 #Output comparsion date
 Write-Host "Changes since $date" -ForegroundColor Green
 
-
 # Output the total lines
 Write-Output "Total lines at ${date}: $totalLinesCommit2"
 Write-Output "Total lines at latest: $totalLinesCommit1"
-
 $added = $totalLinesCommit1 - $totalLinesCommit2
 
 # Calculate percentages
-# Calculate percentages
 $addedPercent = [Math]::Round(($added / $totalLinesCommit2) * 100)
 $changedPercent = [Math]::Round(($changed / $totalLinesCommit2) * 100)
-
-# Calculate the number of unchanged lines
-$unchanged = $totalLinesCommit1 - $added - $changed
-
-# Calculate the percentage of unchanged lines
-$unchangedPercent = [Math]::Round(($unchanged / $totalLinesCommit1) * 100)
-
+$retainedOriginal = [Math]::Round((($totalLinesCommit2-$changed) / $totalLinesCommit1) * 100)
 $changedOrAdded = $added + $changed
 $changedOrAddedPercent = [Math]::Round(($changedOrAdded / $totalLinesCommit2) * 100)
 
@@ -58,6 +50,5 @@ Write-Output "Percentage of lines added: $addedPercent%"
 Write-Output "Percentage of lines changed: $changedPercent%"
 Write-Output "Percentage of lines changed or added: $changedOrAddedPercent%"
 
-# Output the percentage of unchanged lines
-Write-Output "Percentage of lines unchanged: $unchangedPercent%" -ForegroundColor Green
-
+# Color the last output
+Write-Host "Retained original: $retainedOriginal%" -ForegroundColor Green
