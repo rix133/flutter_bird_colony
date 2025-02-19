5fb3ae31d020c7d2910ab6fe926011230a03fbfc
Richard Meitern <richard.meitern@gmail.com>
2025-02-19 11:07:22 +0200

    removing a dependency

--- lib/models/firestoreItemMixin.dart
+++ lib/models/firestoreItemMixin.dart
@@ -86,8 +86,8 @@ class FSItemMixin {
 
       List<List<CellValue>> sortedData = [uniqueHeaders.toList()];
       for (var i = 0; i < data.length; i++) {
-        Map<String, CellValue> rowMap =
-            Map.fromIterables(headers[i].map((h) => h.value), data[i]);
+        Map<String, CellValue> rowMap = Map.fromIterables(
+            headers[i].map((h) => h.value.toString()), data[i]);
         List<CellValue> sortedRow = uniqueHeaders
             .map((h) => rowMap.containsKey(h.value)
                 ? rowMap[h.value]!
